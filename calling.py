80050f7a43475197c11772a6429bb9951d8f424e
TitouanJanod <titouan.janod@uliege.be>
2026-09-09 16:16:07 +0200

    Update calling.py

--- calling.py
+++ calling.py
@@ -7,7 +7,10 @@ Created on Wed Sep  9 16:04:46 2026
 
 from overall_modelling_woIA import overall
 
-
+def get_name_fluids():
+    all_fluids = 1 
+    
+    return all_fluids
 
 selected_fluid = 'R1233zd(E)'
 
